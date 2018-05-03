ce98d04434b2bd511785a5c318db70032cb7d39e
mpab <you@example.com>
2018-05-03 17:40:20 +0200

    moving/renaming bots

---
+++ java/src/main/java/Common/LOG.java
@@ -0,0 +1,11 @@
+package Common;
+
+public class LOG {
+    public static void info(String msg) {
+
+    }
+
+    public static void debug(String msg) {
+
+    }
+}
---
+++ java/src/main/java/NN/NNTester.java
@@ -0,0 +1,31 @@
+package NN;
+
+import Common.Observation;
+import org.json.JSONObject;
+
+public class NNTester extends NNTrainer {
+
+    public void run() {
+
+        conn.open("localhost", 8889);
+        int frame = 1;
+
+        obs = new Observation(getRoundNum() + 1, getNumRounds(), frame++, 0);
+        conn.send(obs.toJson().toString());
+
+        while (true) {
+            int action = getNextAction();
+            proxy.execAction(action);
+            obs = new Observation(getRoundNum() + 1, getNumRounds(), frame++, 0);
+            conn.send(obs.toJson().toString());
+        }
+    }
+
+    private int getNextAction() {
+        String msg = conn.receive();
+        JSONObject jsn = new JSONObject(msg);
+        int action = jsn.getInt("action");
+        //System.out.printf("getNextAction() -> %s\n", proxy.actionToString(action));
+        return action;
+    }
+}
