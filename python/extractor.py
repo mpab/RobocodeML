f9df4c845d3e29020483e0f110efc49ab51a9240
mpab <mick_bythell@hotmail.com>
2018-05-06 11:37:44 +0200

    reverted new pattern extraction

--- python/extractor.py
+++ python/extractor.py
@@ -40,30 +40,48 @@ def extract(features_class, pure_pure):
     return None
 
 
-def make_features_fp(features_class, features_filter):
-    fp = cfg.ensure_fp(cfg.features_root + features_class + '_' + features_filter, cfg.features)
+def make_features_fp(features_class):
+    fp = cfg.ensure_fp(cfg.features_root + features_class, cfg.features)
     return fp
 
 
-def extract_to_csv(obs_list):
+def make_features_fp2(features_class, features_filter):
+    fp = cfg.ensure_fp(cfg.features_root + features_class + '_' + features_filter, cfg.features)
+    return fp
 
+def fn_name(pure_pure, features_class, features_filter, target):
+    return features_class + '_' + features_filter + '_' + target
+
+
+def fn_feature(pure_pure, features_class, features_filter, target):
+    ex1 = extract(features_class, pure_pure)
+
+
+def features_combinations(pure_pure, fn):
+    for features_class in cfg.features_classes:
+        for features_filter in cfg.features_filters:
+            for target in cfg.onehot_targets:
+                return fn(pure_pure, features_class, features_filter, target)
+
+
+def extract_to_csv2(obs_list):
     features_fp = []
 
     # create feature files
     for features_class in cfg.features_classes:
         for features_filter in cfg.features_filters:
-            fp = make_features_fp(features_class, features_filter)
+            fp = make_features_fp(features_class)
             print("creating features file: {}".format(fp))
             features.csv_create(fp)
             features_fp.append(fp)
-            #fp = cfg.ensure_fp(cfg.features_unscanned_root + features_class, cfg.features)
-            #print("creating features file: {}".format(fp))
-            #features.csv_create(fp)
-            #features_fp.append(fp)
+            # fp = cfg.ensure_fp(cfg.features_unscanned_root + features_class, cfg.features)
+            # print("creating features file: {}".format(fp))
+            # features.csv_create(fp)
+            # features_fp.append(fp)
 
     for jsn in obs_list:
         obs = observations.json_to_observation(jsn)
-        
+
         pure_pure = features.observation_to_features(obs)
 
         for features_class in cfg.features_classes:
@@ -74,12 +92,52 @@ def extract_to_csv(obs_list):
                 if out is None:
                     raise RuntimeError("no feature converter for features_class: {}".format(features_class))
 
-                #fp = cfg.ensure_fp(cfg.features_unscanned_root + features_class, cfg.features)
-                #features.csv_append(fp, out)
+                # fp = cfg.ensure_fp(cfg.features_unscanned_root + features_class, cfg.features)
+                # features.csv_append(fp, out)
 
                 if obs.scanned:
-                    fp = make_features_fp(features_class, features_filter)
+                    fp = make_features_fp(features_class)
                     features.csv_append(fp, out)
+
+    for fp in features_fp:
+        size = os.path.getsize(fp)
+        if size < 1024:
+            print("WARNING: length of file {} is {} bytes".format(features_fp, size))
+
+
+def extract_to_csv(obs_list):
+
+    features_fp = []
+
+    # create feature files
+    for features_class in cfg.features_classes:
+        # for features_filter in cfg.features_filters:
+        fp = make_features_fp(features_class)
+        print("creating features file: {}".format(fp))
+        features.csv_create(fp)
+        features_fp.append(fp)
+        #print("creating features file: {}".format(fp))
+        #features.csv_create(fp)
+        #features_fp.append(fp)
+
+    for jsn in obs_list:
+        obs = observations.json_to_observation(jsn)
+        
+        pure_pure = features.observation_to_features(obs)
+
+        for features_class in cfg.features_classes:
+
+            out = extract(features_class, pure_pure)
+
+            if out is None:
+                raise RuntimeError("no feature converter for features_class: {}".format(features_class))
+
+            #fp = cfg.ensure_fp(cfg.features_unscanned_root + features_class, cfg.features)
+            #features.csv_append(fp, out)
+
+            if obs.scanned:
+                fp = make_features_fp(features_class)
+                features.csv_append(fp, out)
 
     for fp in features_fp:
         size = os.path.getsize(fp)
